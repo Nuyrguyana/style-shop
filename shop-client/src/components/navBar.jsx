28901a7ae62b5ad673cdfc2bf7deb87de560d585
Nuyrguyana <enee.kors@mail.ru>
2022-04-23 15:37:38 +0300

    fixed bugs

--- shop-client/src/components/navBar.jsx
+++ shop-client/src/components/navBar.jsx
@@ -1,8 +1,11 @@
 import React, {useContext} from 'react';
 import {Context} from "../index";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import {NavLink} from "react-router-dom";
 import {SHOP_ROUTE} from "../utils/consts";
-import {Button, Container, Nav, Navbar} from "react-bootstrap";
+import {Button} from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 import {observer} from "mobx-react-lite";
 
 const NavBar = observer(() => {
@@ -11,10 +14,12 @@ const NavBar = observer(() => {
         <Navbar bg='dark' variant='dark'>
             <Container>
             <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>OTTOMANN</NavLink>
-            {user._isAuth ?
+            {user.isAuth ?
                 <Nav className='ml-auto' style={{color: 'white'}}>
-                    <Button variant={'outline-light'}>Админ панель</Button>
-                    <Button variant={'outline-light'}>Войти</Button>
+                    <Button
+                        variant={'outline-light'}>Админ панель</Button>
+                    <Button
+                        variant={'outline-light'}>Выйти</Button>
                 </Nav>
                 :
                 <Nav className='ml-auto' style={{color: 'white'}}>
